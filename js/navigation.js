df64e8c223228559e08819e0b2627120359e1ce9
TheTTurkey <22190@rosmini.school.nz>
2026-04-28 22:52:13 +1200

    im not sure

--- js/navigation.js
+++ js/navigation.js
@@ -5,9 +5,6 @@ const playlist_button = document.getElementById('pla_link');
 const about_button = document.getElementById('abo_link');
 const about = document.getElementById('about');
 
-
-
-
 library_button.addEventListener('click', () => {
     library.style.display = "block";
     playlist.style.display = "none";
